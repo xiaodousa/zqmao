b5065a6c9e3bfe96b9a89efe0b0e0385664d62b0
zqmao <719642120@qq.com>
2016-08-24 16:52:47 +0800

    Update js.js

--- docs/js/js.js
+++ docs/js/js.js
@@ -7,13 +7,11 @@ $(function(){
 			for(var i=0;i<data.length;i++){
 				var data_title = data[i].title;
 				var data_link = data[i].link;
-				console.log(data_title);
 				var data_niubi = "";
 				for(var j = 0; j < data_title.length; j++){
 					data_niubi += "<span>" + data_title.substr(j, 1) + "</span>";
 				}
-				console.log(data_niubi);
-				$("logo ul").append("<li><a class='link link--yaku' href=" + data_link + ">" + data_niubi + "</a></li>");
+				$(".logo ul").append("<li><a class='link link--yaku' href=" + data_link + ">" + data_niubi + "</a></li>");
 			}
 		}
 	});
