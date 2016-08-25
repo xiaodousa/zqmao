be753916f059b8b4bf4915082203c27c6e17c5a2
zqmao <719642120@qq.com>
2016-08-25 10:03:52 +0800

    重新写

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,5 +1,7 @@
-$(function(){
+var ZX = {};
 
+$(function(){
+	
     $.ajax({
 		url: "https://zqmao.github.io/blog/list.json",
 		dataType: "json",
@@ -7,13 +9,42 @@ $(function(){
 			for(var i=0;i<data.length;i++){
 				var data_title = data[i].title;
 				var data_link = data[i].link;
+				
 				var data_niubi = "";
-				for(var j = 0; j < data_title.length; j++){
-					data_niubi += "<span>" + data_title.substr(j, 1) + "</span>";
+				for(var j=0;j<data_title.length;j++){
+					data_niubi += "<span>"+data_title.substr(j,1)+"</span>"
 				}
-				$(".logo ul").append("<li><a class='link link--yaku' href=" + data_link + ">" + data_niubi + "</a></li>");
+
+				$(".articleName ul").append("<li><a href=" + data_link + ">" + data_niubi + "</a></li>");
 			}
 		}
 	});
+	
+	ZX.listOutFunc = function(){
+		var box1 = $(".list");
+		var box2 = $(".main_con_left");
+		
+		var bt = $("#list_bt");
+		
+		bt.on("click",function(){
+			var tar = $(this);
+			if(tar.hasClass("out")){
+				tar.removeClass("out");
+				box1.removeClass("out");
+				box2.removeClass("out");
+			}
+			else{
+				tar.addClass("out");
+				box1.addClass("out");
+				box2.addClass("out");
+			}
+			
+			
+		});
+		
+		
+	}();
+	
+	
 
 });
